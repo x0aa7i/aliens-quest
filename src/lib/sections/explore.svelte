8e41bc2641fe736c203bef7345f1921fbef90df9
ABDO <lonihilist@gmail.com>
2025-02-27 19:33:27 +0000

    feat: search

--- src/lib/sections/explore.svelte
+++ src/lib/sections/explore.svelte
@@ -1,14 +1,38 @@
 <script lang="ts">
 	import type { Metadata } from "$lib/solutions";
 
+	import fuzzysort from "fuzzysort";
+
 	import SolutionCard from "$lib/components/solution-card.svelte";
 
-	let { posts }: { posts: Metadata[] } = $props();
+	let data: { posts: Metadata[] } = $props();
+	let search = $state("");
+
+	const posts = $derived.by(() => {
+		if (search.length === 0) {
+			return data.posts;
+		}
+
+		return fuzzysort
+			.go(search, data.posts, {
+				keys: ["title"],
+				limit: 12,
+				threshold: 0.3
+			})
+			.map((post) => post.obj);
+	});
 </script>
 
 <section class="min-h-150 bg-gray-950">
 	<div class="container mx-auto px-8 py-4">
-		<div class="mt-10 h-12 bg-gray-900"></div>
+		<div class="mt-10 h-12 bg-gray-900">
+			<input
+				type="text"
+				placeholder="Search for solutions"
+				class="h-full w-full border-none bg-transparent px-4 py-2 text-gray-100 placeholder:text-gray-500 focus:outline-none"
+				bind:value={search}
+			/>
+		</div>
 
 		<div class="mt-10 grid grid-cols-2 gap-4 lg:grid-cols-3">
 			{#each posts as post}
