6779da0959ca2e931fa9927e1daadcdf45b120c1
ABDO <lonihilist@gmail.com>
2025-03-01 00:47:41 +0000

    feat: dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import "../styles/app.css";
 
+	import { ModeWatcher } from "mode-watcher";
+
 	let { children } = $props();
 </script>
 
+<ModeWatcher defaultMode="dark" />
 {@render children()}
