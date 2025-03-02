e7763e57c7c666794fd7574073b9966050194e65
ABDO <lonihilist@gmail.com>
2025-03-02 14:04:49 +0000

    feat: footer

---
+++ src/lib/sections/footer.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const year = new Date().getFullYear();
+</script>
+
+<footer class="pt-12 md:pt-24">
+	<div class="container mx-auto px-4">
+		<p class="w-full border-t py-5 text-center text-sm text-gray-300">
+			&copy; {year} &mdash; Where is Eveybody.
+		</p>
+	</div>
+</footer>
