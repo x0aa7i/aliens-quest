4f140a17a27e7430193f1f0c08227ef57bf9a5e7
ABDO <lonihilist@gmail.com>
2025-03-11 16:43:07 +0000

    fix: remove unused dependencies

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
 	import "../styles/app.css";
 
-	import { ModeWatcher } from "mode-watcher";
-
 	import Footer from "$lib/sections/footer.svelte";
 
 	let { children } = $props();
 </script>
-
-<ModeWatcher defaultMode="dark" />
 
 {@render children()}
 <Footer />
