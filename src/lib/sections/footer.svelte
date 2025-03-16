a2f5c98941fc1007515996f58d47dee69b10461f
ABDO <abdo.jpeg@gmail.com>
2025-03-16 15:13:09 +0000

    feat: update footer

--- src/lib/sections/footer.svelte
+++ src/lib/sections/footer.svelte
@@ -5,7 +5,14 @@
 <footer class="w-full border-t">
 	<div class="container mx-auto px-4">
 		<p class="w-full py-5 text-center text-sm text-gray-300">
-			&copy; {year} &mdash; Aliens Quest.
+			&copy; {year} Aliens Quest &mdash; Made by
+			<a
+				class="font-medium text-gray-100 hover:underline"
+				href="https://github.com/x0aa7i/"
+				target="_blank"
+				rel="noopener noreferrer">ABDO</a
+			>
+			.
 		</p>
 	</div>
 </footer>
