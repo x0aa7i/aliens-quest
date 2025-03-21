1c607ab80d0baa38c581bd02d5f3a567fab730d2
ABDO <contact.achak@gmail.com>
2025-03-21 16:15:57 +0000

    chore: remove unused files

--- src/lib/server/db/index.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
-
-import { env } from "$env/dynamic/private";
-
-if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
-const client = createClient({ url: env.DATABASE_URL });
-export const db = drizzle(client);
--- src/lib/server/db/schema.ts
+++
@@ -1,6 +0,0 @@
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
-export const user = sqliteTable("user", {
-	id: integer("id").primaryKey(),
-	age: integer("age")
-});
