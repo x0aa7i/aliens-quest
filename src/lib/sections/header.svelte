98198871c5383894b9fc41b530d8b945f57ff0f0
ABDO <contact.achak@gmail.com>
2025-05-02 21:32:55 +0100

    fix: logo size adjustments

--- src/lib/sections/header.svelte
+++ src/lib/sections/header.svelte
@@ -13,12 +13,13 @@
 	];
 </script>
 
-<header class={["container mx-auto px-4 py-4 md:px-8", _class]}>
+<header class={["container mx-auto px-4 py-4 lg:px-8", _class]}>
 	<nav class="mx-auto flex flex-wrap items-center justify-between">
 		<a href="/" class="shrink-0 text-xl font-bold text-gray-50">
-			<img src="/logo.svg" width="87" height="28" alt="logo" class="h-8 w-auto" />
+			<img src="/logo.svg" width="87" height="28" alt="logo" class="h-7 w-auto" />
 		</a>
-		<ul class="flex items-center gap-6 text-sm font-medium uppercase text-gray-300 md:gap-8">
+
+		<ul class="flex items-center gap-5 text-sm font-medium uppercase text-gray-300 md:gap-8">
 			{#each navLinks as link (link.label)}
 				<li><a href={link.href} class="transition-colors hover:text-gray-50">{link.label}</a></li>
 			{/each}
