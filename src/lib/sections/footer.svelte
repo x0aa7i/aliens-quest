9a609c1e97508104bb9ece32b9f3e255a6b983d8
ABDO <contact.achak@gmail.com>
2025-05-02 21:32:28 +0100

    feat: update footer link

--- src/lib/sections/footer.svelte
+++ src/lib/sections/footer.svelte
@@ -9,14 +9,15 @@
 <footer class={cn("w-full border-t", _class)}>
 	<div class="container mx-auto px-4">
 		<p class="w-full py-5 text-center text-sm text-gray-300">
-			&copy; {year} Aliens Quest &mdash; Made by
+			&copy; {year} Aliens Quest &mdash;
 			<a
-				class="font-medium text-gray-100 hover:underline"
-				href="https://achak.dev"
+				class="inline-flex items-center gap-1 font-medium text-gray-100 hover:underline"
+				href="https://github.com/x0aa7i/aliens-quest"
 				target="_blank"
-				rel="noopener noreferrer">ABDO</a
+				rel="noopener noreferrer"
 			>
-			.
+				GitHub
+			</a>
 		</p>
 	</div>
 </footer>
