b80bc4e73a423252c9dd8d4bdb6006528eb164d9
ABDO <contact.achak@gmail.com>
2025-05-08 19:51:47 +0100

    chore: add unplugin-icons to dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
+import Icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
+	plugins: [sveltekit(), tailwindcss(), Icons({ compiler: "svelte" })],
 	server: {
 		fs: {
 			// Allow serving files from one level up to the project root
