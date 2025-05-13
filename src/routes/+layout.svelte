d7d1432675c20b6333235c9c08234b5f0e37019a
ABDO <contact.achak@gmail.com>
2025-05-13 01:53:16 +0100

    feat: add view transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
+	import { onNavigate } from "$app/navigation";
+
 	import "../styles/app.css";
 
 	let { children } = $props();
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 {@render children()}
